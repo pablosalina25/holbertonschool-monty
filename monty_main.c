6414232ebc9e2a7e0bb764492745b98dce06a0f0
pablosalina25 <pablosalina25@gmail.com>
2024-01-03 08:01:07 -0800

    intento

---
+++ monty_main.c
@@ -0,0 +1,24 @@
+nclude <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+}
+	stack_t;
+
+int main(int numero_argumentos, char *argumentos[])
+{
+	FILE *archivo;
+	char *linea = NULL;
+	size_t longitud_linea = 0;
+	stack_t *pila = NULL;
+	int numero_linea = 0;
+
+	if (numero_argumentos != 2)
+	{
+	fprintf(stderr, "Uso: programa archivo\n");
+	exit(EXIT_FAILURE);
+	}
+	
+	return (0);
+}
