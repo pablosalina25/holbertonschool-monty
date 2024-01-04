777b96efd550ad92c3f9fc6af8cefbe780555e0c
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-04 15:32:27 -0300

    Update push.c

--- push.c
+++ push.c
@@ -6,35 +6,46 @@
  * @line_number: Line number in the file
  * @value: Value to be added to the stack
  */
-void push(stack_t **stack, int line_number, int value)
+void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node;
-
-	if (!stack)
-	{
-		fprintf(stderr, "L%d: stack is NULL\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	if (!isdigit(value) && value[0] != '-' && !isdigit(value[1]))
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+	stack_t *new_node = NULL;
+	char *token = NULL;
+	int i;
 
 	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		fprintf(stderr, "L%d: malloc failed\n", line_number);
+		fprintf(stderr, "Error: malloc failed\n");
+		if (*stack)
+			free_stack(*stack);
 		exit(EXIT_FAILURE);
 	}
 
-	new_node->n = atoi(value);
+	token = strtok(NULL, " \t\n");
+	if (!token)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		free(new_node), free_stack(*stack), exit(EXIT_FAILURE);
+	}
+	for (i = 0; token[i] && token[0] != '-'; i++)
+	{
+		if (!isdigit(token[i]))
+		{
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			free(new_node), free_stack(*stack), exit(EXIT_FAILURE);
+		}
+	}
+	new_node->n = atoi(token);
 	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
+	if (!*stack)
+	{
+		new_node->next = NULL;
+		*stack = new_node;
+	}
+	else
+	{
+		new_node->next = *stack;
 		(*stack)->prev = new_node;
-
-	*stack = new_node;
+		*stack = new_node;
+	}
 }
