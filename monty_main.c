31f97cf36ccf5d94ca56d93fd3daef8e2fddc361
pablosalina25 <pablosalina25@gmail.com>
2024-01-05 17:10:45 -0800

    error fixed

--- monty_main.c
+++ monty_main.c
@@ -1,17 +1,12 @@
 #include "monty.h"
 
 int main(void)
-/**
- * main - Entry point for the Monty interpreter
- *
- * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
- */
 {
-	stack_t *stack = NULL;
+    stack_t *stack = NULL;
 
-	push(&stack, 1);
-	push(&stack, 2);
-	push(&stack, 3);
-	pall(&stack);
-	return (0);
+    push(&stack, 1);
+    push(&stack, 2);
+    push(&stack, 3);
+    pall(&stack, 0); // Debes proporcionar ambos argumentos
+    return (0);
 }
