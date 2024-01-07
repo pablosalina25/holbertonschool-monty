5a5be29ff6d6f43d87a79785e60a5e0d5888f044
pablosalina25 <pablosalina25@gmail.com>
2024-01-07 15:33:31 -0800

    pint

--- pint.c
+++ pint.c
@@ -1,17 +1,20 @@
 #include "monty.h"
+
 /**
-* _pint - The opcode pint prints a value at
-* the top of the stack, followed by a new line
-* @stack: stack where work will be done.
-* @line_number: number of lines.
-* Return: 0 if the first element is printed
-**/
+ * _pint - The opcode pint prints a value at
+ * the top of the stack, followed by a new line
+ * @stack: stack where work will be done.
+ * @line_number: number of lines.
+ * Return: 0 if the first element is printed
+ **/
 void _pint(stack_t **stack, unsigned int line_number)
 {
-	if ((*stack)->next == NULL && (*stack)->prev == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	printf("%d\n", ((*stack)->n)); 
+
+	stack_t *top = *stack;
+	printf("%d\n", top->n);
 }
