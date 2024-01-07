764b54eaa082564c669a8eed9b0b7308211203bb
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-07 20:57:50 -0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -30,7 +30,7 @@ void pall(stack_t **stack, unsigned int n);
 void swapTopElements(stack_t **stackHead, unsigned int lineNumber);
 void addTopTwoElements(stack_t **stackHead, unsigned int lineNumber);
 void printTopElement(stack_t **stackHead, unsigned int lineNumber);
-void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 int is_integer(const char *str);
 stack_t *new_Node(int value);
 
