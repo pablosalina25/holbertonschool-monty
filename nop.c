e4feb7c6706a845a9479819bf06ec30a11202be2
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-08 19:36:01 -0300

    Update nop.c

--- nop.c
+++ nop.c
@@ -4,7 +4,7 @@
  * nop - doesn’t do anything.
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
- * @data: pointer to void data 
+ * @data: pointer to void data
  */
 void nop(stack_t **stack, unsigned int line_number, void *data)
 {
