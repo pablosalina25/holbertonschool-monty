e674d3af07cd0b9da5666f4b852d9806f72cc3e6
pablosalina25 <135639187+pablosalina25@users.noreply.github.com>
2024-01-09 20:23:29 -0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,28 +1,20 @@
 #include "monty.h"
 #include <unistd.h>
+
 /**
- * pall - print all the values of the stack
- * @stack: double pointer to the stack
- * @n: data to print
- * Return: nothing
- **/
+ * pall - Print all the values of the stack.
+ * @stack: Double pointer to the stack.
+ * @n: Unused parameter.
+ * Return: Nothing.
+ */
 void pall(stack_t **stack, unsigned int n)
 {
-	stack_t *current = NULL;
-	(void)n;
-	stack_t *current = *stack;
+    stack_t *current = *stack;
+    (void)n;
 
-	(void)line_number;
-
-	current = *stack;
-	if (current == NULL)
-	return;
-
-	while (current != NULL)
-	{
-		dprintf(STDOUT_FILENO, "%d\n", current->n);
-		current = current->next;
-	printf("%d\n", current->n);
-	current = current->next;
-	}
+    while (current != NULL)
+    {
+        dprintf(STDOUT_FILENO, "%d\n", current->n);
+        current = current->next;
+    }
 }
