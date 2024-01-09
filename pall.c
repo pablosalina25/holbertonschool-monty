62c8d3be7de598f4cff646690ff97ed6a5635637
Diego Piñeyro <124789249+Diego29012@users.noreply.github.com>
2024-01-09 19:54:49 -0300

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,4 +1,24 @@
 #include "monty.h"
+#include <stdio.h>
+
+/**
+ * free_stack - frees the memory allocated for the stack
+ * @stack: double pointer to the stack
+ */
+void free_stack(stack_t **stack)
+{
+    stack_t *current = *stack;
+
+    while (current != NULL)
+    {
+        stack_t *temp = current;
+        current = current->next;
+        free(temp);
+    }
+
+    *stack = NULL;
+}
+
 /**
  * pall - Print all the values of the stack.
  * @stack: Double pointer to the stack.
@@ -7,16 +27,18 @@
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
+    stack_t *current = *stack;
 
-	(void)line_number;
+    (void)line_number;
 
-	if (current == NULL)
-	return;
+    if (current == NULL)
+        return;
 
-	while (current != NULL)
-	{
-	printf("%d\n", current->n);
-	current = current->next;
-	}
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+
+    free_stack(stack);
 }
