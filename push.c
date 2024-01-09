81dcbdc835fb53cb774341ed097b2ab47d578228
pablosalina25 <135639187+pablosalina25@users.noreply.github.com>
2024-01-09 20:15:10 -0300

    Update push.c

--- push.c
+++ push.c
@@ -7,39 +7,31 @@
  * @line_number: line number of the command
  */
 void push(stack_t **stack, unsigned int line_number)
-void push(stack_t **stack)
 {
-	stack_t *new = NULL;
-	int value = 0;
-	(void)line_number;
+    stack_t *new = NULL;
+    int value = 0;
 
-	value = 42;
-	if (cmd.content == NULL)
-	{
-	fprintf(stderr, "Error: Can't push without a value\n");
-	exit(EXIT_FAILURE);
-	}
+    if (cmd.content == NULL)
+    {
+        fprintf(stderr, "Error: Can't push without a value\n");
+        exit(EXIT_FAILURE);
+    }
 
-	stack_t *new_Node(int value);
-	value = atoi(cmd.content);
+    value = atoi(cmd.content);
 
-	new = new_Node(value);
-	new = malloc(sizeof(stack_t));
-	if (new == NULL)
-	{
-	fprintf(stderr, "Error: malloc failed\n");
-	exit(EXIT_FAILURE);
-	}
+    new = malloc(sizeof(stack_t));
+    if (new == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	new->n = value;
-	new->prev = NULL;
-	new->next = *stack;
+    new->n = value;
+    new->prev = NULL;
+    new->next = *stack;
 
-	if (*stack != NULL)
-	(*stack)->prev = new;
+    if (*stack != NULL)
+        (*stack)->prev = new;
 
-	*stack = new;
-}
-
-    *stack = NULL;
+    *stack = new;
 }
