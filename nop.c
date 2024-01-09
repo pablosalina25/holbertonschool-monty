0eb03361ec261c499ed9b337156e1b1fca9943be
pablosalina25 <pablosalina25@gmail.com>
2024-01-09 06:26:26 -0800

    bbug

--- nop.c
+++ nop.c
@@ -1,14 +1,11 @@
 #include "monty.h"
-
 /**
- * nop - doesn’t do anything.
- * @stack: double pointer to head of stack
- * @line_number: line number of current operation
- * @data: pointer to void data
- */
-void nop(stack_t **stack, unsigned int line_number, void *data)
+ *nop - doesn’t do anything.
+ *@stack: double pointer to head of stack
+ *@line_number: line number of current operation
+*/
+void nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
 	(void)line_number;
-	(void)data;
 }
