70095e1fee42001d423f27df62435756654c8f22
pablosalina25 <pablosalina25@gmail.com>
2024-01-09 05:36:12 -0800

    nop

--- nop.c
+++ nop.c
@@ -4,11 +4,16 @@
  * nop - doesn’t do anything.
  * @stack: double pointer to head of stack
  * @line_number: line number of current operation
- * @data: pointer to void data
  */
-void nop(stack_t **stack, unsigned int line_number, void *data)
+void nop(stack_t **stack, unsigned int line_number)
 {
-	(void)stack;
-	(void)line_number;
-	(void)data;
+    if (stack != NULL && *stack != NULL)
+    {
+        /* Código para nop si es necesario */
+    }
+    else
+    {
+        fprintf(stderr, "L%u: Error - Stack is empty\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 }
